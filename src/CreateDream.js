d80dfe7b43565d577954e91ff080d0e27c90dd38
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-04-17 22:08:43 -0700

    Add four inputs for createDream

--- src/CreateDream.js
+++ src/CreateDream.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+import TextInput from './TextInput';
 
 const CreateDream = (props) => {
+  const [email, setEmail] = useState('');
+  const [elementIDs, setElementIDs] = useState('');
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   return (
     <div>
-      <Form>
-        <Form.Group controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control 
-            type="email" 
-            placeholder="Enter email" 
-            />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-      </Form>
+      <TextInput 
+        type="email" 
+        controlId="CreateDreamEmail" 
+        label="Email address"
+        placeholder="Enter email"
+        subText="We'll never share your email with anyone else." 
+        value={email} 
+        handleChange={setEmail}/>
+
+      <TextInput 
+        type="text" 
+        controlId="CreateDreamElementIDs" 
+        label="Element IDs"
+        placeholder="Enter element IDs"
+        subText="Comma seperated" 
+        value={elementIDs} 
+        handleChange={setElementIDs}/>
+
+      <TextInput 
+        type="text" 
+        controlId="CreateDreamTitle" 
+        label="Title"
+        placeholder="Enter dream title"
+        value={title} 
+        handleChange={setTitle}/>
+
+      <TextInput 
+        type="text" 
+        controlId="CreateDreamDesc" 
+        label="Description"
+        placeholder="Enter dream description"
+        subText="Optional" 
+        value={desc} 
+        handleChange={setDesc}/>
     </div>
   )
 }
---
+++ src/TextInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const TextInput = (props) => {
+  const {controlId, label, type, placeholder, subText, value, handleChange} = props;
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId={controlId}>
+          <Form.Label>{label}</Form.Label>
+          <Form.Control 
+            type={type}
+            placeholder={placeholder} 
+            value={value}
+            onChange={(e) => handleChange(e.target.value)}
+            />
+          <Form.Text className="text-muted">
+            {subText}
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </div>
+  )
+}
+
+export default TextInput;
