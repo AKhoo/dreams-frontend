53c5a1bba60037f4f8aa883901c6ad3cb820ee3c
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-05-06 11:11:16 -0700

    Add button to refresh random dream

--- src/SendDream.js
+++ src/SendDream.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 import { getRandomDream, setSelectedDream } from './actions';
 
 const SendDream = (props) => {
@@ -8,16 +9,21 @@ const SendDream = (props) => {
   const selectedDreamElementName = selectedDream.relationships ? elementsData[selectedDream.relationships.elements.data[0].id].attributes.name : null;
   const selectedDreamElementDesc = selectedDream.relationships ? elementsData[selectedDream.relationships.elements.data[0].id].attributes.commentary : null;
 
-  useEffect(() => {
+  const getAndStoreRandomDream = () => {
     getRandomDream()
       .then(({data}) => {
         storeSelectedDream(data.data);
       });
+  }
+
+  useEffect(() => {
+    getAndStoreRandomDream();
   }, []);
 
   return (
     <div>
       <h1>Send a Dream</h1>
+      <Button variant="light" onClick={getAndStoreRandomDream}>Next Dream</Button>
       <div>
         <Image src="https://img.icons8.com/ios-glyphs/30/000000/corgi.png" rounded />
         <p>{selectedDreamElementName}</p>
@@ -26,6 +32,7 @@ const SendDream = (props) => {
         <p>Someone dreamed of a {selectedDreamElementName}!</p>
         <p>{selectedDreamElementDesc}</p>
         <p>For a donation of $5.00, we’ll reveal the details of the dream to the beneficiary, so they can benefit from this good omen.</p>
+        <p>Dream ID: {selectedDream.id}</p>
       </div>
     </div>
   )
