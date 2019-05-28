624e4a837042d6b7bbf045a5bf20e807f2c9fd18
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-05-28 12:21:51 -0700

    Moved submission form from CreateDream to Modal

--- src/components/other/CreateDreamModal.js
+++ src/components/other/CreateDreamModal.js
@@ -1,21 +1,74 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+
+import FormInput from '../forms/FormInput';
+import FormTextarea from '../forms/FormTextarea';
+
+import { postDream } from '../../actions';
 
 const CreateDreamModal = props => {
-  const { selectedElement } = props;
+  const { selectedElement, addSuccessMessage } = props;
+
+  const [email, setEmail] = useState('');
+  const [description, setDescription] = useState('');  
+
   return (
     <Modal
-      show={selectedElement.id}
+      show
       centered
       dialogClassName="CreateDreamModal"
     >
       <Modal.Dialog>
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Finish Journaling</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          <p>You dreamed of a {selectedElement.name}? Tell us more!</p>
+          <p>“Donate” a good omen by sending us the details of your dream. If it’s usable, we’ll let others “buy” the omen in exchange for a small donation to a non-profit organization. </p>
+          <p>We keep your dream secret, and only reveal the details when the buyer has made a donation to charity. By donating a dream, you help others with their dreams!</p>
+
+            <Form
+            onSubmit={e => {
+              e.preventDefault();
+              postDream({
+                email,
+                element_ids: selectedElement.id,
+                description,
+              }).then(() => {
+                addSuccessMessage('Dream successfully submitted. Thank you!');
+                setEmail('');
+                setDescription('');
+              });
+            }}
+          >
+            <FormInput
+              type="email"
+              isRequired={true}
+              controlId="CreateDreamEmail"
+              label="Email address"
+              placeholder="Enter email"
+              subText="We'll never share your email with anyone else."
+              value={email}
+              handleChange={setEmail}
+            />
+
+            <FormTextarea
+              rows={3}
+              isRequired={true}
+              controlId="CreateDreamDesc"
+              label="Description"
+              placeholder="Enter dream description"
+              subText="Describe your dream"
+              value={description}
+              handleChange={setDescription}
+            />
+
+            <Button type="submit" variant="primary">
+              Donate My Dream
+            </Button>
+          </Form>
+
         </Modal.Body>
 
         <Modal.Footer>
--- src/components/pages/CreateDream.js
+++ src/components/pages/CreateDream.js
@@ -5,37 +5,21 @@ import { Button, Image, Form } from 'react-bootstrap';
 import SpinnerModal from '../other/SpinnerModal';
 import CreateDreamModal from '../other/CreateDreamModal';
 import GuideElement from '../other/GuideElement';
-import FormInput from '../forms/FormInput';
-import FormTextarea from '../forms/FormTextarea';
-import ElementSelect from '../forms/ElementSelect';
 
-import { getAndStoreElements, postDream, addMessage } from '../../actions';
+import { getAndStoreElements, addMessage } from '../../actions';
 
 const CreateDream = props => {
   const { elementsData, addSuccessMessage } = props;
 
   const elementsArray = elementsData.data ? Object.values(elementsData.data) : null;
 
-  const elementsDataFirstElement = elementsData.data
-    ? elementsData.data[Object.keys(elementsData.data)[0]]
-    : null;
-
-  const [email, setEmail] = useState('');
   const [selectedElement, setSelectedElement] = useState({name: null,id: null});
-  const [description, setDescription] = useState('');
   
   useEffect(() => {
     if (!elementsData.data) {
       getAndStoreElements();
     };
   }, []);
-
-  // if (elementsData.data & !selectedElement.id) {
-  //   setSelectedElement({
-  //     name: elementsDataFirstElement.attributes.name,
-  //     id: elementsDataFirstElement.id,
-  //   });
-  // }
   
   return (
     <div>
@@ -55,74 +39,7 @@ const CreateDream = props => {
           : null}
       </div>
 
-      <CreateDreamModal selectedElement={selectedElement}/>
-
-      <ElementSelect
-        controlId="CreateDreamElement"
-        label="What was the most prominent symbol in your dream?"
-        options={elementsData.data ? elementsData.data : {}}
-        selectedElement={selectedElement}
-        handleChange={setSelectedElement}
-      />
-
-      <div className="createDreamDetails">
-        <Image
-          src="https://img.icons8.com/ios-glyphs/30/000000/corgi.png"
-          rounded
-        />
-        <h2>{selectedElement.name}</h2>
-        <p>
-          {selectedElement.id
-            ? elementsData.data[selectedElement.id].attributes.commentary
-            : ''}
-        </p>
-
-        <p>
-          If you had a good omen, you can donate your dream to give someone else
-          good luck!
-        </p>
-
-        <Form
-          onSubmit={e => {
-            e.preventDefault();
-            postDream({
-              email,
-              element_ids: selectedElement.id,
-              description,
-            }).then(() => {
-              addSuccessMessage('Dream successfully submitted. Thank you!');
-              setEmail('');
-              setDescription('');
-            });
-          }}
-        >
-          <FormInput
-            type="email"
-            isRequired={true}
-            controlId="CreateDreamEmail"
-            label="Email address"
-            placeholder="Enter email"
-            subText="We'll never share your email with anyone else."
-            value={email}
-            handleChange={setEmail}
-          />
-
-          <FormTextarea
-            rows={3}
-            isRequired={true}
-            controlId="CreateDreamDesc"
-            label="Description"
-            placeholder="Enter dream description"
-            subText="Describe your dream"
-            value={description}
-            handleChange={setDescription}
-          />
-
-          <Button type="submit" variant="primary">
-            Donate My Dream
-          </Button>
-        </Form>
-      </div>
+      {selectedElement.id && <CreateDreamModal selectedElement={selectedElement} addSuccessMessage={addSuccessMessage}/>}
     </div>
   );
 };
