421aec38091820b0d41d74923f80a1a9c1dbd7b1
David Wen <Dawenster@users.noreply.github.com>
2019-06-09 15:58:06 -0700

    Fix element image sizing

--- src/components/other/GuideElement.js
+++ src/components/other/GuideElement.js
@@ -2,16 +2,25 @@ import React from 'react';
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 
 const GuideElement = props => {
-  const { element, withButton, handleClick, setShowModal } = props;
+  const {
+    element,
+    isLastElement,
+    withButton,
+    handleClick,
+    setShowModal,
+  } = props;
   return (
-    <Container>
+    <Container className={isLastElement ? '' : 'mb-4'}>
       <Row>
-        <Col xs="auto">
-          <div className="circle">
-            <Image src={element.attributes.image_url} />
-          </div>
+        <Col xs={3} md={2}>
+          <Image
+            className="elementImage"
+            src={element.attributes.image_url}
+            fluid
+            roundedCircle
+          />
         </Col>
-        <Col>
+        <Col xs={9} md={10}>
           <p className="elementName">{element.attributes.name}</p>
           <p>{element.attributes.commentary}</p>
           {withButton && (
